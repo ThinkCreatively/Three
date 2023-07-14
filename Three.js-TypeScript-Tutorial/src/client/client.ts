d207891bddd422116fab97389e107f45cbde496a
James Moore <james3.moore@dish.com>
2023-07-14 13:55:12 -0600

    ~ Implemented fps and memory stat ~ Implemented dat gui

--- Three.js-TypeScript-Tutorial/src/client/client.ts
+++ Three.js-TypeScript-Tutorial/src/client/client.ts
@@ -1,51 +1,69 @@
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Stats from 'three/examples/jsm/libs/stats.module'
+import { GUI } from 'dat.gui'
 
-const scene = new THREE.Scene();
+const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-camera.position.z = 2;
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+)
+camera.position.z = 2
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
 
-new OrbitControls(camera, renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement)
+//controls.addEventListener('change', render)
 
-const geometry = new THREE.BoxGeometry();
+const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshBasicMaterial({
-  color: 0x00ff00,
-  wireframe: true,
-});
+    color: 0x00ff00,
+    wireframe: true,
+})
 
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
 
-const onWindowResize = () => {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  render();
-};
+window.addEventListener('resize', onWindowResize, false)
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    render()
+}
 
-window.addEventListener("resize", onWindowResize, false);
+const stats = new Stats()
+document.body.appendChild(stats.dom)
 
-const render = () => {
-  renderer.render(scene, camera);
-};
+const gui = new GUI()
+const cubeFolder = gui.addFolder('Cube')
+cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
+cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
+cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
+cubeFolder.open()
+const cameraFolder = gui.addFolder('Camera')
+cameraFolder.add(camera.position, 'z', 0, 10)
+cameraFolder.open()
 
-const animate = () => {
-  requestAnimationFrame(animate);
+function animate() {
+    requestAnimationFrame(animate)
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+    //cube.rotation.x += 0.01
+    //cube.rotation.y += 0.01
 
-  render();
-};
+    render()
 
-animate();
+    stats.update()
+}
+
+function render() {
+    renderer.render(scene, camera)
+}
+
+animate()
+//render()
