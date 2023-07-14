f862eb5a2303bbbd512d3cda28915b66d9f68140
James Moore <james3.moore@dish.com>
2023-07-14 15:25:48 -0600

    ~ Updated title ~ Added comments to break up code ~ Going through 3D Heirarchy

--- Three.js-TypeScript-Tutorial/src/client/client.ts
+++ Three.js-TypeScript-Tutorial/src/client/client.ts
@@ -1,69 +1,93 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import Stats from 'three/examples/jsm/libs/stats.module'
-import { GUI } from 'dat.gui'
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import Stats from "three/examples/jsm/libs/stats.module";
+import { GUI } from "dat.gui";
 
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-)
-camera.position.z = 2
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+camera.position.z = 2;
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-const controls = new OrbitControls(camera, renderer.domElement)
+new OrbitControls(camera, renderer.domElement);
+// const controls = new OrbitControls(camera, renderer.domElement);
 //controls.addEventListener('change', render)
 
-const geometry = new THREE.BoxGeometry()
+// * Cube
+
+const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({
-    color: 0x00ff00,
-    wireframe: true,
-})
+  color: 0x00ff00,
+  wireframe: true,
+});
 
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+const cube = new THREE.Mesh(geometry, material);
 
-window.addEventListener('resize', onWindowResize, false)
+window.addEventListener("resize", onWindowResize, false);
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  render();
 }
 
-const stats = new Stats()
-document.body.appendChild(stats.dom)
+// * GUI and Stats 
 
-const gui = new GUI()
-const cubeFolder = gui.addFolder('Cube')
-cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
-cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
-cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
-cubeFolder.open()
-const cameraFolder = gui.addFolder('Camera')
-cameraFolder.add(camera.position, 'z', 0, 10)
-cameraFolder.open()
+const stats = new Stats();
+document.body.appendChild(stats.dom);
 
-function animate() {
-    requestAnimationFrame(animate)
+const gui = new GUI();
+const cubeFolder = gui.addFolder("Cube");
 
-    //cube.rotation.x += 0.01
-    //cube.rotation.y += 0.01
+const cubeRotationFolder = cubeFolder.addFolder("Rotation");
+cubeRotationFolder.add(cube.rotation, "x", 0, Math.PI * 2);
+cubeRotationFolder.add(cube.rotation, "y", 0, Math.PI * 2);
+cubeRotationFolder.add(cube.rotation, "z", 0, Math.PI * 2);
+cubeFolder.open();
+cubeRotationFolder.open();
 
-    render()
+const cubePositionFolder = cubeFolder.addFolder("Position");
+cubePositionFolder.add(cube.position, "x", -10, 10, 2);
+cubePositionFolder.add(cube.position, "y", -10, 10, 2);
+cubePositionFolder.add(cube.position, "z", -10, 10, 2);
+cubeFolder.open();
+cubePositionFolder.open();
 
-    stats.update()
-}
+const cubeScaleFolder = cubeFolder.addFolder("Scale");
+cubeScaleFolder.add(cube.scale, "x", -5, 5);
+cubeScaleFolder.add(cube.scale, "y", -5, 5);
+cubeScaleFolder.add(cube.scale, "z", -5, 5);
+cubeFolder.add(cube, "visible");
+cubeFolder.open();
+cubeScaleFolder.open();
 
-function render() {
-    renderer.render(scene, camera)
-}
+scene.add(new THREE.AxesHelper(5));
+scene.add(cube);
 
-animate()
+// * Animate and Render
+
+const animate = () => {
+  requestAnimationFrame(animate);
+
+  //cube.rotation.x += 0.01
+  //cube.rotation.y += 0.01
+
+  render();
+
+  stats.update();
+};
+
+const render = () => {
+  renderer.render(scene, camera);
+};
+
+animate();
 //render()
